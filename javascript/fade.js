fb4cf6dc57a26d5d066e5b624fb86248c895ec3e
Kenzie Swinford <swinfordkenzie@gmail.com>
2021-01-22 14:57:01 -0600

    custom scrollbar, change color of navigation when scrolling, and other styling

---
+++ javascript/fade.js
@@ -0,0 +1,7 @@
+document.body.classList.add('fade');
+
+document.addEventListener("DOMContentLoaded", () => {
+    window.setTimeout(function() {
+        document.body.className = '';
+    }, 230);
+});
